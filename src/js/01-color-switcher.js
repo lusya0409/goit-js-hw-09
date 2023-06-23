e2b233fe9017f0069bc00882064b6eaff6d7497b
lusya0409 <lusya0409@gmail.com>
2023-06-23 22:35:37 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const buttonStart = document.querySelector('[data-start');
+const buttonStop = document.querySelector('[data-stop');
+buttonStop.disabled = true;
+let timerId = null;
 
+buttonStart.addEventListener('click', onClickButtonStart);
+buttonStop.addEventListener('click', onClickButtonStop);
+
+function onClickButtonStart(e) {
+  e.target.disabled = true;
+  buttonStop.disabled = false;
+
+  timerId = setInterval(() => {
+    setBodyBgColor();
+  }, 1000);
+}
+function onClickButtonStop(e) {
+  console.log(e);
+  e.target.disabled = true;
+  buttonStart.disabled = false;
+
+  clearInterval(timerId);
+}
+function setBodyBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
